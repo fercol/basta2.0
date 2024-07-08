2f7572f189c63d490d20e5c4e42f8f807f2fba15
Fernando <fernando_colchero@eva.mpg.de>
2024-07-08 17:11:07 +0200

    added function multibasta

--- pkg/R/basta.R
+++ pkg/R/basta.R
@@ -994,8 +994,9 @@ basta.default <- function(object, dataType = "CMR",
   return(bastaFinal)
 }
 
-# A.3) plotting BaSTA outputs:
-# ---------------------------- #
+# A.3) plotting and printing BaSTA outputs:
+# ----------------------------------------- #
+# Plotting:
 plot.basta <- function(x, plot.type = "traces", trace.name = "theta",
                        densities = FALSE, noCIs = FALSE, minSurv = NULL, ...) {
   # User par settings:
@@ -1520,8 +1521,7 @@ plot.basta <- function(x, plot.type = "traces", trace.name = "theta",
   }
 }
 
-# A.4) Printing BaSTA outputs:
-# ---------------------------- #
+# Printing:
 print.basta <- function(x, ...) {
   extraArgs <- list(...)
   if (length(extraArgs) > 0) {
@@ -1556,8 +1556,7 @@ print.basta <- function(x, ...) {
   }
 }
 
-# A.5) Summary for BaSTA outputs:
-# ------------------------------- #
+# Summary:
 summary.basta <- function(object, ...){
     extraArgs       <- list(...)
     if (length(extraArgs) > 0) {
@@ -1658,7 +1657,8 @@ summary.basta <- function(object, ...){
     return(invisible(ans))
   }
 
-# A.6) construct capture-recapture matrix:
+# A.4) construct capture-recapture matrix:
+# ---------------------------------------- #
 CensusToCaptHist <- function(ID, d, dformat = "%Y", timeInt = "Y") {
   # Check data
   if(!inherits(ID, "character")) {
@@ -1715,6 +1715,135 @@ CensusToCaptHist <- function(ID, d, dformat = "%Y", timeInt = "Y") {
   dmat <- data.frame(ID = uniID, mat)
   
   return(dmat)
+}
+
+# A.7) Multi-BaSTA functions:
+# --------------------------- #
+# Main multibasta:
+multibasta <- function(object, dataType = "CMR", models, 
+                       shapes, ...) {
+  argList <- list(...)
+  if (is.null(argList$nsim)) nsim <- 2 else nsim <- argList$nsim
+  mods <- rbind(data.frame(model = "EX", shape = "simple"),
+                expand.grid(model = c("GO", "WE", "LO"), 
+                            shape = c("simple", "Makeham", "bathtub")))
+  
+  modsAbr <- rbind(data.frame(model = "Ex", shape = "Si"),
+                   expand.grid(model = c("Go", "We", "Lo"), 
+                               shape = c("Si", "Ma", "Bt")))
+  if (missing(models)) models <- c("EX", "GO", "WE", "LO")
+  if (missing(shapes)) shapes <- c("simple", "Makeham", "bathtub")
+  idIncl <- which(mods$model %in% models & mods$shape %in% shapes)
+  mods <- mods[idIncl, ]
+  modsAbr <- modsAbr[idIncl, ]
+  nmod <- nrow(mods)
+  modNames <- paste(modsAbr$model, modsAbr$shape, sep = ".")
+  if (nsim > 1) DIC <- TRUE else DIC <- FALSE
+  if (DIC) {
+    dics <- matrix(NA, nmod, 5, dimnames = 
+                     list(modNames, c("D.ave", "D.mode", "pD", "k", "DIC")))
+  }
+  runList <- list()
+  for (mod in 1:nmod) {
+    updt <- sprintf("Run number %s, model: %s", mod, modNames[mod])
+    cat(paste("\n", paste(rep("-", nchar(updt)), collapse = ""), sep = ""))
+    cat(sprintf("\n%s\n", updt))
+    cat(paste(paste(rep("-", nchar(updt)), collapse = ""), "\n", sep = ""))
+    if (is.null(argList$nsim)) {
+      out <- basta(object, dataType = dataType, studyStart, studyEnd, 
+                   model = mods$model[mod], 
+                   shape = mods$shape[mod], nsim = nsim, ...)
+    } else {
+      out <- basta(object, dataType = dataType,  model = mods$model[mod], 
+                   shape = mods$shape[mod], ...)
+    }
+    runList[[modNames[mod]]] <- out
+    if (DIC & out$DIC[1] != "Not calculated") {
+      dics[mod, ] <- out$DIC
+    }
+  }
+  if (DIC) {
+    idNa <- which(is.na(dics[, "DIC"]))
+    idnNa <- which(!is.na(dics[, "DIC"]))
+    idModFit <- c(idnNa[sort.int(dics[idnNa, 5], index.return = TRUE)$ix], 
+                  idNa)
+    if (is.na(idModFit[1])) idModFit <- 0
+    modFit <- data.frame(mods, dics)[idModFit, ]
+    DICdiff <- modFit$DIC - modFit$DIC[1]
+    Rank <- 1:nmod
+    modFit <- data.frame(modFit, DICdiff, Rank)
+  } else {
+    modFit <- data.frame(DICs = "Not Calculated")
+    idModFit <- NA
+  }
+  results <- list(runs = runList[idModFit], DICs = modFit, models = mods)
+  class(results) <- "multibasta"
+  return(results)
+}
+
+# Printing:
+print.multibasta <- function(x, ...) {
+  argList <- list(...)
+  if (is.null(argList$digits)) digits <- 3 else digits <- argList$digits
+  cat("\nDICs:\n")
+  print(x$DICs, digits = digits)
+}
+
+# Summary:
+summary.multibasta <- function(object, ...) {
+  argList <- list(...)
+  if (is.null(argList$digits)) digits <- 3 else digits <- argList$digits
+  if ("version" %in% names(object$runs[[1]])) {
+    cat(sprintf("\nBaSTA version %s\n", object$runs[[1]]$version))
+  }
+  
+  cat("\nCall:\n")
+  cat(paste("Covars. structure \t\t: ", object$runs[[1]]$modelSpecs[3], "\n", 
+            sep = ""))
+  cat(paste("Minimum age       \t\t: ", object$runs[[1]]$modelSpecs[4], "\n", 
+            sep = ""))
+  cat(paste("Cat. covars.      \t\t: ", object$runs[[1]]$modelSpecs[5], "\n", 
+            sep = ""))
+  cat(paste("Cont. covars.     \t\t: ", object$runs[[1]]$modelSpecs[6], "\n", 
+            collapse = ""))
+  
+  cat("\nModel settings:\n")
+  print(object$runs[[1]]$set)
+  
+  cat("\nDICs:\n")
+  print(object$DICs, digits = digits)
+}
+
+# Extract coefficients:
+coef.multibasta <- function(object, showAll = FALSE, ...) {
+  argList <- list(...)
+  if (is.null(argList$digits)) digits <- 3 else digits <- argList$digits
+  ans <- list()
+  if (inherits(showAll,  "logical")) {
+    if (showAll) nmod <- 1:length(object$runs) else nmod <- 1
+  } else if (inherits(showAll, "numeric")) {
+    if (length(showAll) == 1) {
+      nmod <- 1:showAll
+    } else {
+      nmod <- showAll
+    }
+    nmod <- nmod[nmod <= length(object$runs)]
+  } else {
+    stop("\nArgument 'showAll' should be logical or an integer.\n", 
+         call. = FALSE)
+  }
+  modNames <- names(object$runs)
+  for (mod in nmod) {
+    updt <- sprintf("DIC rank %s, model: %s", as.character(object$DICs$Rank)[mod], 
+                    modNames[mod])
+    cat(paste("\n", paste(rep("-", nchar(updt)), collapse = ""), sep = ""))
+    cat(sprintf("\n%s\n", updt))
+    cat(paste(paste(rep("-", nchar(updt)), collapse = ""), "\n", sep = ""))
+    print.default(object$runs[[mod]]$coefficients, digits = digits)
+    ans$coefficients[[modNames[mod]]] <- object$runs[[mod]]$coefficients
+  }
+  ans$DICs <- object$DICs
+  return(invisible(ans))
 }
 
 # ============================================ #
